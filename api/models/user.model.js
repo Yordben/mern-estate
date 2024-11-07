b23166b28e689164bd254971d6f10830ceb3dba7
yordi beneberu <yordi.beneberu@gmail.com>
2024-11-07 16:05:37 +0300

    type correction

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,17 +4,17 @@ import { type } from "os";
 const userScheme = new mongoose.Schema({
     username:{
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     email:{
         type: String,
-        require: true,
+        required: true,
         unique: true
     },
     password:{
         type: String,
-        require: true,
+        required: true,
         
     },
 }, { timestamps: true})
